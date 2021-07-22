d7e8445e5a31900deb7ed2eeff4e0f04f938ca71
Mengqi Lu <emily.lmq960524@gmail.com>
2021-07-22 15:51:56 -0700

    modiefied Py 3

--- proj.py
+++ proj.py
@@ -1,3 +1,40 @@
 import numpy as np
-
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import math
+%matplotlib inline
+import os
+os.environ["PYSPARK_PYTHON"] = "python3"
+
+
+# Part1: Data ETL and Data Exploration
+
+from pyspark.sql import SparkSession
+spark = SparkSession \
+    .builder \
+    .appName("moive analysis") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+    
+movies_df = spark.read.load("/FileStore/tables/movies.csv", format='csv', header = True)
+ratings_df = spark.read.load("/FileStore/tables/ratings.csv", format='csv', header = True)
+links_df = spark.read.load("/FileStore/tables/links.csv", format='csv', header = True)
+tags_df = spark.read.load("/FileStore/tables/tags.csv", format='csv', header = True)
+
+movies_df.show(5)
+ratings_df.show(5)
+links_df.show(5)
+tags_df.show(5)
+
+# some basic counts
+tmp1 = ratings_df.groupBy("userID").count().toPandas()['count'].min()
+tmp2 = ratings_df.groupBy("movieId").count().toPandas()['count'].min()
+print('For the users that rated movies and the movies that were rated:')
+print('Minimum number of ratings per user is {}'.format(tmp1))
+print('Minimum number of ratings per movie is {}'.format(tmp2))
+
+tmp1 = sum(ratings_df.groupBy("movieId").count().toPandas()['count'] == 1)
+tmp2 = ratings_df.select('movieId').distinct().count()
+print('{} out of {} movies are rated by only one user'.format(tmp1, tmp2))
+
